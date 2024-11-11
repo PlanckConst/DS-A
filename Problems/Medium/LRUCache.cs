1d6e7b20f33f7dc1ce7cd4774f172c868f61a56f
PlanckConst <brandonchin.dev@gmail.com>
2024-11-11 16:20:57 -0500

    LRU Cache Finalized and Test Passed

--- Problems/Medium/LRUCache.cs
+++ Problems/Medium/LRUCache.cs
@@ -4,56 +4,63 @@ namespace Problems.Medium
 {
     public class LRUCache
     {
-        private readonly Dictionary<int, LinkedListNode<KeyValuePair<int, int>>> _index;
-        private readonly int _capacity;
-        private readonly LinkedList<KeyValuePair<int, int>> _cache;
+        private Dictionary<int, LinkedListNode<int?>> _index;
+        private int _capacity;
+        private LinkedList<int?> _cache;
 
         public LRUCache(int capacity)
         {
             _capacity = capacity;
-            _index = [];
-            _cache = new LinkedList<KeyValuePair<int, int>>();
+            _index = new Dictionary<int, LinkedListNode<int?>>();
+            _cache = new LinkedList<int?>();
         }
 
         public int Get(int key)
         {
-            if (!_index.TryGetValue(key, out var node))
-                return -1;
+            //Search for the value too see if key exists
+            if (!_index.TryGetValue(key, out LinkedListNode<int?>? node)) return -1;
+            //Check if key is found, but node has a null value (buffer)
+            if (!node.Value.HasValue) return -1;
 
-            // Move the accessed node to the front (most recently used)
             _cache.Remove(node);
             _cache.AddFirst(node);
-            return node.Value.Value;
+            return node.Value!.Value;
         }
 
         public void Put(int key, int value)
         {
-            if (_index.TryGetValue(key, out var node))
+            //Buffer: check if key exists and if value is null. If true then remove the key.
+            if (_index.ContainsKey(key) && !_index[key].Value.HasValue) _index.Remove(key);
+
+            LinkedListNode<int?> node = new LinkedListNode<int?>(value);
+
+            //Checks if key does not exist and can add it
+            if (_index.TryAdd(key, node))
             {
-                // Update the value and move to front
-                node.Value = new KeyValuePair<int, int>(key, value);
+                //Check if cache is full
+                if (_cache.Count + 1 > _capacity)
+                {
+                    //Set last node value to null
+                    _cache.Last!.Value = null;
+                    _cache.RemoveLast();
+                }
+                _cache.AddFirst(node);
+            }
+            //Key exists so update the node value and bring it to front of cache
+            else
+            {
+                node = _index[key];
+                node.Value = value;
                 _cache.Remove(node);
                 _cache.AddFirst(node);
             }
-            else
-            {
-                if (_cache.Count >= _capacity)
-                {
-                    // Remove the least recently used item
-                    var lru = _cache.Last;
-                    if (lru != null)
-                    {
-                        _index.Remove(lru.Value.Key);
-                        _cache.RemoveLast();
-                    }
-                }
-
-                // Add the new key-value pair
-                var kvp = new KeyValuePair<int, int>(key, value);
-                var newNode = new LinkedListNode<KeyValuePair<int, int>>(kvp);
-                _cache.AddFirst(newNode);
-                _index[key] = newNode;
-            }
         }
     }
+
+    /**
+     * Your LRUCache object will be instantiated and called as such:
+     * LRUCache obj = new LRUCache(capacity);
+     * int param_1 = obj.Get(key);
+     * obj.Put(key,value);
+     */
 }
