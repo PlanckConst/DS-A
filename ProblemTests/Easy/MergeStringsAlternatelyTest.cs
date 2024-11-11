f781f86435efaf08ba4630bc60f4c50518a8d77f
PlanckConst <brandonchin.dev@gmail.com>
2024-11-11 10:02:57 -0500

    Add project files.

---
+++ ProblemTests/Easy/MergeStringsAlternatelyTest.cs
@@ -0,0 +1,23 @@
+﻿using Problems.Easy;
+
+namespace ProblemTests.Easy
+{
+    [TestFixture]
+    public class MergeStringsAlternatelyTest
+    {
+        [Test]
+        public void DefaultTest()
+        {
+            Assert.That(Leet.MergeAlternately("abc", "pqr"), Is.EqualTo("apbqcr"));
+            Assert.That(Leet.MergeAlternately("ab", "pqrs"), Is.EqualTo("apbqrs"));
+            Assert.That(Leet.MergeAlternately("abcd", "pq"), Is.EqualTo("apbqcd"));
+        }
+        [Test]
+        public void MyTest()
+        {
+            Assert.That(Leet.MergeAlternately("acefg", "bd"), Is.EqualTo("abcdefg"));
+            Assert.That(Leet.MergeAlternately("acegi", "bdfhj"), Is.EqualTo("abcdefghij"));
+            Assert.That(Leet.MergeAlternately("acegi", "bdfhjk"), Is.EqualTo("abcdefghijk"));
+        }
+    }
+}
---
+++ Problems/Easy/MergeStringsAlternately.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace Problems.Easy
+{
+    public class Leet
+    {
+        public static string MergeAlternately(string word1, string word2)
+        {
+            //Instantiate a StringBuilder Class result string, capacity = word2 + word1 length, but maximum capacity is the max constraint which is 200
+            StringBuilder resultString = new(word1.Length + word2.Length, 200);
+
+            int maxLength = Math.Max(word1.Length, word2.Length);
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                if (i < word1.Length)
+                {
+                    resultString.Append(word1[i]);
+                }
+                if (i < word2.Length)
+                {
+                    resultString.Append(word2[i]);
+                }
+            }
+            return resultString.ToString();
+        }
+    }
+}
