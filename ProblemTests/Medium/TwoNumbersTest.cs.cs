148d0ef5bccf82ccfe337c98373c116c7c0754aa
PlanckConst <brandonchin.dev@gmail.com>
2024-11-12 09:30:17 -0500

    Cleaned up new() expression and unused variables

--- ProblemTests/Medium/TwoNumbersTest.cs.cs
+++ ProblemTests/Medium/TwoNumbersTest.cs.cs
@@ -11,7 +11,7 @@ namespace ProblemTests.Medium
         {
             ListNode dummy = new(0);
             ListNode current = dummy;
-            foreach (var num in numbers)
+            foreach (int num in numbers)
             {
                 current.next = new ListNode(num);
                 current = current.next;
@@ -22,7 +22,7 @@ namespace ProblemTests.Medium
         // Helper method to convert a linked list to an array
         private static int[] ListToArray(ListNode node)
         {
-            var result = new List<int>();
+            List<int> result = [];
             while (node != null)
             {
                 result.Add(node.val);
@@ -35,12 +35,12 @@ namespace ProblemTests.Medium
         public void Test_SampleInput1()
         {
             // Input: [2,4,3], [5,6,4]
-            var l1 = CreateList([2, 4, 3]);
-            var l2 = CreateList([5, 6, 4]);
-            var expected = new int[] { 7, 0, 8 };
+            ListNode l1 = CreateList([2, 4, 3]);
+            ListNode l2 = CreateList([5, 6, 4]);
+            int[] expected = new int[] { 7, 0, 8 };
 
-            var result = Leet2.AddTwoNumbers(l1, l2);
-            var resultArray = ListToArray(result);
+            ListNode result = Leet2.AddTwoNumbers(l1, l2);
+            int[] resultArray = ListToArray(result);
 
             Assert.That(resultArray, Is.EqualTo(expected));
         }
@@ -49,12 +49,12 @@ namespace ProblemTests.Medium
         public void Test_DifferentLengths()
         {
             // Input: [9,9,9,9,9,9,9], [9,9,9,9]
-            var l1 = CreateList([9, 9, 9, 9, 9, 9, 9]);
-            var l2 = CreateList([9, 9, 9, 9]);
-            var expected = new int[] { 8, 9, 9, 9, 0, 0, 0, 1 };
+            ListNode l1 = CreateList([9, 9, 9, 9, 9, 9, 9]);
+            ListNode l2 = CreateList([9, 9, 9, 9]);
+            int[] expected = new int[] { 8, 9, 9, 9, 0, 0, 0, 1 };
 
-            var result = Leet2.AddTwoNumbers(l1, l2);
-            var resultArray = ListToArray(result);
+            ListNode result = Leet2.AddTwoNumbers(l1, l2);
+            int[] resultArray = ListToArray(result);
 
             Assert.That(resultArray, Is.EqualTo(expected));
         }
@@ -63,12 +63,12 @@ namespace ProblemTests.Medium
         public void Test_ZeroInputs()
         {
             // Input: [0], [0]
-            var l1 = CreateList([0]);
-            var l2 = CreateList([0]);
-            var expected = new int[] { 0 };
+            ListNode l1 = CreateList([0]);
+            ListNode l2 = CreateList([0]);
+            int[] expected = new int[] { 0 };
 
-            var result = Leet2.AddTwoNumbers(l1, l2);
-            var resultArray = ListToArray(result);
+            ListNode result = Leet2.AddTwoNumbers(l1, l2);
+            int[] resultArray = ListToArray(result);
 
             Assert.That(resultArray, Is.EqualTo(expected));
         }
@@ -77,12 +77,12 @@ namespace ProblemTests.Medium
         public void Test_SingleDigitCarryOver()
         {
             // Input: [5], [5]
-            var l1 = CreateList([5]);
-            var l2 = CreateList([5]);
-            var expected = new int[] { 0, 1 };
+            ListNode l1 = CreateList([5]);
+            ListNode l2 = CreateList([5]);
+            int[] expected = new int[] { 0, 1 };
 
-            var result = Leet2.AddTwoNumbers(l1, l2);
-            var resultArray = ListToArray(result);
+            ListNode result = Leet2.AddTwoNumbers(l1, l2);
+            int[] resultArray = ListToArray(result);
 
             Assert.That(resultArray, Is.EqualTo(expected));
         }
@@ -94,7 +94,7 @@ namespace ProblemTests.Medium
             ListNode? l1 = null;
             ListNode? l2 = null;
 
-            var result = Leet2.AddTwoNumbers(l1!, l2!);
+            ListNode result = Leet2.AddTwoNumbers(l1!, l2!);
 
             Assert.That(result, Is.Null);
         }
