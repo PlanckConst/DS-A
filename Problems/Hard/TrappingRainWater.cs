07a8555ea6bd19e114849d04488aeb994a803253
PlanckConst <brandonchin.dev@gmail.com>
2024-11-12 14:57:41 -0500

    Completed Trapping Rain Water

--- Problems/Hard/TrappingRainWater.cs
+++ Problems/Hard/TrappingRainWater.cs
@@ -1,12 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Problems.Hard
 {
-    internal class TrappingRainWater
+    public class Leet42
     {
+        public static int Trap(int[] height)
+        {
+            int left = 0, right = height.Length - 1;
+            int ans = 0;
+            int left_max = 0, right_max = 0;
+            while (left < right)
+            {
+                if (height[left] < height[right])
+                {
+                    left_max = Math.Max(left_max, height[left]);
+                    ans += left_max - height[left];
+                    ++left;
+                }
+                else
+                {
+                    right_max = Math.Max(right_max, height[right]);
+                    ans += right_max - height[right];
+                    --right;
+                }
+            }
+            return ans;
+        }
     }
 }
