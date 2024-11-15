f403007abdf26b4b08c4e5ceb3c1ebab932fbf62
PlanckConst <brandonchin.dev@gmail.com>
2024-11-15 15:51:40 -0500

    Merge Intervals Completed

---
+++ Problems/Medium/MergeIntervals.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problems.Medium
+{
+    public class MergeIntervals
+    {
+        public static int[][] Merge(int[][] intervals)
+        {
+            if (intervals.Length <= 1)
+                return intervals;
+
+            // Sort intervals based on the start time
+            Array.Sort(intervals, (a, b) => a[0].CompareTo(b[0]));
+
+            var merged = new List<int[]>();
+            var currentInterval = intervals[0];
+            merged.Add(currentInterval);
+
+            foreach (var interval in intervals)
+            {
+                int currentEnd = currentInterval[1];
+                int nextStart = interval[0];
+                int nextEnd = interval[1];
+
+                // Check if intervals overlap
+                if (currentEnd >= nextStart)
+                {
+                    // Merge the intervals
+                    currentInterval[1] = Math.Max(currentEnd, nextEnd);
+                }
+                else
+                {
+                    // Move to the next interval
+                    currentInterval = interval;
+                    merged.Add(currentInterval);
+                }
+            }
+
+            return merged.ToArray();
+        }
+    }
+}
